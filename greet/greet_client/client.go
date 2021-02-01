bfcb1e01b64eb4d56daff2fe83e5b006d37c934a
juyoung <wanke0524@gmail.com>
2021-02-01 22:15:34 +0900

    Bi-Directional Streaming(Greet)

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -25,7 +25,8 @@ func main() {
 	//fmt.Printf("Created client: %f", c)
 	//doUnary(c)
 	//doServerStreaming(c)
-	doClientStreaming(c)
+	//doClientStreaming(c)
+	doBiDiStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -119,4 +120,72 @@ func doClientStreaming(c greetpb.GreetServiceClient) {
 		log.Fatalf("error while recieving response form LongGreet: %v", err)
 	}
 	log.Printf(res.GetResult())
+}
+
+func doBiDiStreaming(c greetpb.GreetServiceClient) {
+	fmt.Printf("Starting to do a Bi-Directional Streaming RPC...\n")
+
+	//we create a stream by invoking the client
+	stream, err := c.GreetEveryone(context.Background())
+	if err != nil {
+		log.Fatalf("error while creating stream: %v", err)
+		return
+	}
+	request := []*greetpb.GreetEveryoneRequest{
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "juyoung",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "hyesun",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "heejin",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "jisoo",
+			},
+		},
+		&greetpb.GreetEveryoneRequest{
+			Greeting: &greetpb.Greeting{
+				FirstName: "eunhye",
+			},
+		},
+	}
+
+	waitc := make(chan struct{})
+	//we send a bunch of messages to the client(go routine)
+	go func() {
+		//function to send a bunch of messages
+		for _, req := range request {
+			fmt.Printf("Sending request: %v\n", req)
+			stream.Send(req)
+			time.Sleep(100 * time.Millisecond)
+		}
+		stream.CloseSend()
+	}()
+	//we receive a bunch of messages from the client(go routine)
+	go func() {
+		//function to receive a bunch of messages
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error while receiving: %v", err)
+				break
+			}
+			fmt.Printf("Received: %v\n", res.GetResult())
+		}
+		close(waitc)
+	}()
+	//block until everything is done
+	<-waitc
 }
